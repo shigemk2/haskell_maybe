bbe9fdba7b6e5abb48d56c5601ab7482fc01b57e
shigemk2 <i.am.shige@gmail.com>
2015-02-01 18:08:41 +0900

    再実装2 doを使わない

--- re-implement1.hs
+++ re-implement1.hs
@@ -1,11 +1,13 @@
 -- returnにMaybeモナドの型注釈をつけるとJustと同じ
 -- b x = return $ x
 
-a `bind` b = do
-    a' <- a
-    b a'
+(Just a) `bind` f = f a
+Nothing `bind` _ = Nothing
+
+-- a `bind` b =
+--     let (Just a') = a
+--     in b a'
 
 main = do
     print $ Just 1 `bind` \a -> Just $ a * 2
     print $ Just 1 `bind` \a -> Nothing `bind` \b -> Just $ a * b
-    print $ Just 1 `bind` Just
