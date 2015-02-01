2b92dabc8d0a25f20656c18111a1aa80b7f25291
shigemk2 <i.am.shige@gmail.com>
2015-02-01 18:35:58 +0900

    階乗のapplicativeスタイル

--- factorial.hs
+++ factorial.hs
@@ -1,8 +1,15 @@
 import Control.Applicative
 
 fact 0 = Just 1
+-- fact n | n > 0     = (n *) <$> fact (n - 1)
+-- fact n | n > 0     = do
+--                      n' <- fact (n - 1)
+--                      return $ n * n'
+-- fact n | n > 0     = fact (n - 1) >>= \n' -> return $ n * n'
+-- f(g(x)) -> f $ g x -> (f . g) x
+-- fact n | n > 0     = fact (n - 1) >>= \n' -> (return . (n *)) n'
+-- fact n | n > 0     = fact (n - 1) >>= return . (n *)
 fact n | n > 0     = (n *) <$> fact (n - 1)
--- fact n | n > 0     = n * fact (n - 1) -- こういうふうに書くとエラーになる
        | otherwise = Nothing
 
 main = do
