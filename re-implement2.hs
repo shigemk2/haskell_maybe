49e67a4186081e6616b6f6f1e93fdf23a77c6a4a
shigemk2 <i.am.shige@gmail.com>
2015-02-01 21:16:53 +0900

    mapMaybe 再実装 ベース

--- re-implement2.hs
+++ re-implement2.hs
@@ -5,6 +5,10 @@ fact 0 = Just 1
 fact n | n > 0     = (n *) <$> fact (n - 1)
        | otherwise = Nothing
 
+facts n = ( map      fact [n, n - 1, n - 2]
+          , mapMaybe' fact [n, n - 1, n - 2]
+          )
+
 mapMaybe' _ [] = []
 mapMaybe' f (x:xs) = do
     let a = f x
@@ -12,8 +16,9 @@ mapMaybe' f (x:xs) = do
     a' : mapMaybe' f xs
 
 main = do
+    print $ facts 3
+    print $ facts 2
+    print $ facts 1  -- cで失敗
+    print $ facts 0  -- bで失敗
     print $ map fact [3,2,1]
-    -- let a = fact 3
-    --     (Just a') = a
-    -- print a'
     print $ mapMaybe' fact [3,2,1]
