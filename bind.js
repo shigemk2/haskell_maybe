2de21da923118acc63693e1f7555ab990165326c
shigemk2 <i.am.shige@gmail.com>
2015-02-01 16:52:18 +0900

    iojsをためそう

--- bind.js
+++ bind.js
@@ -1,8 +1,13 @@
 console.log(function () {
     var ret = [];
-    for (var a of [1]) {      // 値がある
+    for (var a of [2,3,4]) {      // 値がある
         ret.push(a * 2);      // 処理される
     }
+    // var a = [2,3,4];
+    // for (var i=0; i<a.length; i++ ) {      // 値がある
+    // for (var i in a) {      // 値がある
+    //     ret.push(a[i] * 2);      // 処理される
+    // }
     return ret;
 }());
 console.log(function () {
