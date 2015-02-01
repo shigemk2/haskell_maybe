5785f476cd0f4890453d705e7a4b99c86d3edf67
shigemk2 <i.am.shige@gmail.com>
2015-02-01 19:10:35 +0900

    mapMaybe

---
+++ mapMaybe.hs
@@ -0,0 +1,16 @@
+import Control.Applicative
+import Data.Maybe
+
+fact 0 = Just 1
+fact n | n > 0     = (n *) <$> fact (n - 1)
+       | otherwise = Nothing
+
+facts n = ( map      fact [n, n - 1, n - 2]
+          , mapMaybe fact [n, n - 1, n - 2]
+          )
+
+main = do
+    print $ facts 3
+    print $ facts 2
+    print $ facts 1  -- cで失敗
+    print $ facts 0  -- bで失敗
