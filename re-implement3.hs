f19df82e24b9849783ed34fe216fee5ffdc88143
shigemk2 <i.am.shige@gmail.com>
2015-02-01 21:26:30 +0900

    mapMaybe foldr実装 ベース

---
+++ re-implement3.hs
@@ -0,0 +1,23 @@
+import Control.Applicative
+import Data.Maybe
+
+fact 0 = Just 1
+fact n | n > 0     = (n *) <$> fact (n - 1)
+       | otherwise = Nothing
+
+facts n = ( map      fact [n, n - 1, n - 2]
+          , mapMaybe' fact [n, n - 1, n - 2]
+          )
+
+mapMaybe' _ [] = []
+mapMaybe' f (x:xs) = case f x of
+    Just y -> y : mapMaybe' f xs
+    Nothing -> mapMaybe' f xs
+
+main = do
+    print $ facts 3
+    print $ facts 2
+    print $ facts 1  -- cで失敗
+    print $ facts 0  -- bで失敗
+    print $ map fact [3,2,1]
+    print $ mapMaybe' fact [3,2,1]
